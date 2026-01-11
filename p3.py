09854445d745926fa92d9a5292e4ec407ef74f9e
perumallameghana <23211a12c9@bvrit.ac.in>
2026-01-11 14:12:46 +0530

    Initialcommit

---
+++ p3.py
@@ -0,0 +1,32 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+data = []
+
+@app.get("/")
+def home():
+    return {"message": "Basic FastAPI CRUD"}
+
+@app.post("/add")
+def add_item(name: str):
+    data.append(name)
+    return {"added": name}
+
+@app.get("/list")
+def list_items():
+    return data
+
+@app.put("/update/{index}")
+def update_item(index: int, name: str):
+    if index < len(data):
+        data[index] = name
+        return {"updated": name}
+    return {"error": "Index out of range"}
+
+@app.delete("/delete/{index}")
+def delete_item(index: int):
+    if index < len(data):
+        removed = data.pop(index)
+        return {"deleted": removed}
+    return {"error": "Index out of range"}
